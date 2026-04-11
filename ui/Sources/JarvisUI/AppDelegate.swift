50963ab72bbb508cc1e271c6bb557424b99c1352
Zeffut <thomas2.de-sousa@etu.u-pec.fr>
2026-04-11 12:56:18 +0200

    feat: AppDelegate + main entry point

---
+++ ui/Sources/JarvisUI/AppDelegate.swift
@@ -0,0 +1,36 @@
+import AppKit
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var panel: JarvisPanel?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Pas d'icône dans le Dock
+        NSApp.setActivationPolicy(.accessory)
+
+        panel = JarvisPanel()
+        SocketListener.shared.start()
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleMessage(_:)),
+            name: SocketListener.messageNotification,
+            object: nil
+        )
+    }
+
+    @objc private func handleMessage(_ notification: Notification) {
+        guard let msg = notification.object as? JarvisMessage else { return }
+
+        if msg.state == "standby" {
+            panel?.close()
+        } else if panel?.isVisible == false {
+            panel?.open()
+        }
+
+        panel?.update(state: msg.state, amplitude: msg.amplitude)
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        unlink("/tmp/jarvis-ui.sock")
+    }
+}
